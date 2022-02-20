dc0528a426c2a47f49286499e33c78445c6fb9ff
OviyaaB <oviyaa.mnb@gmail.com>
2022-02-19 21:07:37 -0500

    Add project files

---
+++ pickle_sample.py
@@ -0,0 +1,9 @@
+import pickle
+import sys
+
+if sys.version_info.major == 2:
+    X,y,Xtest,ytest = pickle.load(open('sample.pickle','rb'))
+else:
+    X,y,Xtest,ytest = pickle.load(open('sample.pickle','rb'),encoding = 'latin1')
+#qdaLearn(X,y)
+print(X)
